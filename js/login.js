073369db2c12e57787ef5fbb394cb406e7bafeb9
bbourassa <bbourassa@umass.edu>
2020-10-31 14:21:25 -0400

    added comments to help ease process of changes

--- js/login.js
+++ js/login.js
@@ -1,5 +1,13 @@
 'use strict';
 
+/*
+FOR NOW: double checks that a username and password 
+         have been entered. Enables sign-in button only
+         when username and password field have
+         inputs.
+FUTURE:  will check that user login info is valid
+         before allowing sign-in to occur
+*/
 function checkValidation() {
     let setActive = true;
     for(let input of inputs) {
@@ -13,14 +21,19 @@ function checkValidation() {
     }
 }
 
+/*
+FOR NOW: redirects to the Personal Calendar
+         page on sign-in
+*/
 function redirectToCal() {
     window.location = '../html/personalcal.html';
 }
 
-function redirectToPasswordRet() {
-    window.location = '../html/passwordretrieve.html';
-}
-
+/*
+FOR NOW: -sets methods for signInButton
+         -sets validation checks on user input
+FUTURE:  TBD
+*/
 const signInButton = document.getElementById('signIn');
 signInButton.addEventListener('click', redirectToCal);
 
@@ -28,4 +41,3 @@ const inputs = document.getElementsByTagName('input');
 for(let input of inputs) {
     input.addEventListener('keyup', checkValidation);
 }
-console.log('hit');
--- js/signup.js
+++ js/signup.js
@@ -1,5 +1,11 @@
 'use strict';
 
+/*
+THIS WAS MOCK SINGUP USER INFO DATA OBJECT
+CAN WE REMOVE THIS NOW??
+I THINK WE MAY BE ABLE TO OR WE MAY
+USE IT TO PASS THE DATA - NOT SURE
+*/
 let signUpInfo = {
     firstName: '',
     lastName: '', 
@@ -8,6 +14,17 @@ let signUpInfo = {
     password: ''
 };
 
+/*
+FOR NOW: double checks that password and the confirmed
+         password are the same before allowing the
+         user to sign up
+FUTURE:  add some indication to user - right now it
+         is just button disabled but we should let
+         user know why --> i plan on adding this when
+         we add in tooltips for our forms which i have
+         in my head as its own separate task once we have
+         everything
+*/
 function checkPasswords() {
     const password1 = document.getElementById('password');
     const password2 = document.getElementById('passwordConfirmation');
@@ -18,6 +35,16 @@ function checkPasswords() {
     }
 }
 
+/*
+FOR NOW: checks if all form inputs have been filled 
+         out by the user then sets signUpInfo values
+         when all are true --> enables sign in button
+         when all fields are filled out
+FUTURE:  will check that user login info is valid
+         before allowing sign-in to occur --> this 
+         should also show an indication to show
+         why sign in button may still be disabled
+*/
 function checkValidation() {
     let setActive = true;
     for(let input of inputs) {
@@ -39,10 +66,17 @@ function checkValidation() {
     }
 }
 
+/*
+redirect function on sign in to
+personal calendar page
+*/
 function redirectToCal() {
     window.location = '../html/personalcal.html';
 }
 
+/*
+initial page setup
+*/
 const inputs = document.getElementsByTagName('input');
 for(let input of inputs) {
     input.addEventListener('keyup', checkValidation);
