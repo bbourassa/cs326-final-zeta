9e67ae408cfdaff23d857cc23d9f8dc8d347e5d0
bbourassa <bbourassa@umass.edu>
2020-11-15 18:23:38 -0500

    fixed db stuff to work with correct db calls and all are active

--- server/db/users.js
+++ server/db/users.js
@@ -3,7 +3,6 @@
 const db = require('../app.js').db;
 
 db.none('CREATE TABLE IF NOT EXISTS users(id INTEGER PRIMARY KEY, username VARCHAR, firstName VARCHAR, lastName VARCHAR, email VARCHAR, password_val VARCHAR, calendar_id INTEGER UNIQUE, notifications TEXT );');
-//db.none('INSERT INTO public."users"(id, username, firstName, lastName, email, password_val, calendar_id, notifications) VALUES(0, \'LifeOnTrack\', \'no first name\', \'no last name\', \'lifeontrack@gmail.com\', \'password\', 0, \'example notif\');');
 
 const users = [];
 
@@ -12,15 +11,14 @@ const users = [];
 // 	res.redirect('/personalcal.html');
 // };
 
-exports.list = function(req, res) {
-	//res.end(JSON.stringify(db.any('SELECT * FROM public."users";')));
+exports.list = async function(req, res) {
+    res.json(await db.any('SELECT * FROM public."users";'));
 };
 
 exports.create = function(req, res) {
 	res.sendStatus(201);
 	let lastId = db.any('SELECT MAX(id) FROM public."users";');
 	let newId = lastId + 1;
-	/*
     let username = req.body.username;
     let firstName = req.body.firstName;
     let lastName = req.body.lastName;
@@ -29,13 +27,12 @@ exports.create = function(req, res) {
     let calendar_id = req.body.calendar_id;
     let notifications = req.body.notifications;
     db.none('INSERT INTO public."user"(id, username, firstName, lastName, email, password_val, calendar_id, notifications) VALUES($1, $2, $3, $4, $5, $6, $7, $8);', [newId, username, firstName, lastName, email, password_val, calendar_id, notifications]);
-    */
 };
 
-exports.load = function(req, res, next) {
+exports.load = async function(req, res, next) {
 	const id = parseInt(req.params.user, 10);
 
-	res.end(JSON.stringify(db.any('SELECT * FROM public."users" WHERE id=$1;', [id])));
+	res.json(await db.any('SELECT * FROM public."users" WHERE id=$1;', [id]));
 
 	req.user = users[id];
 	if (req.user) {
@@ -49,10 +46,10 @@ exports.load = function(req, res, next) {
 };
 
 //check if the username exists
-exports.find = function(req, res) {
+exports.find = async function(req, res) {
 	const username = req.params.username;
 	try{
-		res.end(JSON.stringify(db.any('SELECT * FROM public."users" WHERE username=$1;', [username])));
+		res.json(await db.any('SELECT * FROM public."users" WHERE username=$1;', [username]));
 	} catch(e){
 		return false;
 	}
@@ -61,21 +58,17 @@ exports.find = function(req, res) {
 
 
 exports.remove = function(req, res) {
-	/*
     let userId = req.params.user;
     db.none('DELETE from public."users" WHERE id=$1;', [userId]);
-    */
 	res.sendStatus(204);
 };
 
 //NEEDS TO BE WORKED MORE - MEGHAN CAN YOU ALTER THIS TO WORK CORRECTLY?
 //I FEEL YOU PROBABLY KNOW MORE ABOUT THIS FUNCTION
-exports.listSubscribed = function(req, res) {
-	/*
+exports.listSubscribed = async function(req, res) {
     let userId = req.body.id;
-    res.end(JSON.stringify(db.any(SELECT * FROM public."subscriptions" INNER JOIN public."calendars" ON calendars.id = subscriptions.calendar_id WHERE user_id=$1;', [userId])));
-    */
-	res.json(req.subs.map(sub => users[sub.user_id]));
+    res.json(await db.any('SELECT * FROM public."subscriptions" INNER JOIN public."calendars" ON calendars.id = subscriptions.calendar_id WHERE user_id=$1;', [userId]));
+	//res.json(req.subs.map(sub => users[sub.user_id]));
 };
 
 //POST notification
