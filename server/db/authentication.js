e311a1a263e42b066e52c1eb9e6ac6892caad2ae
Meghan <arnol24m@mtholyoke.edu>
2020-11-16 18:12:55 -0500

    working on authentication

--- server/db/authentication.js
+++ server/db/authentication.js
@@ -23,22 +23,7 @@ const session = {
 	saveUninitialized : false
 };
 
-//configure passport
-// const strategy = new LocalStrategy(
-// 	async(username, password, done) => {
-// 		if(!findUser(username)){
-// 			return done(null, false, { 'message': 'Wrong username or password'});
-// 		}
-// 		if(!checkCreds(username, password)){
-// 			//creates a 2 sec delay between failed attempts
-// 			await new Promise((r) => setTimeout(r, 2000));
-// 			return done(null, false, {'message':'Wrong username or password'});
-// 		}
-// 		//create a user object, associated w/ user_id
-// 		// currently: user object is username string
-// 		return done(null, username);
-// 	}
-// );
+
 //app configuration :AKA: MAGIC CODE, DO NOT CHANGE
 app.use(expressSession(session));
 // passport.use(strategy);
@@ -49,65 +34,31 @@ app.use(passport.session());
 exports.findU =  async function findUser(username){
 	// let exists;
 	try {
-        const user = JSON.stringify(db.any('SELECT * FROM public."users" WHERE username=$1;', [username]));
-        return true;
-    } catch(e){
-        console.log(e);
-        return false;
+		const user = JSON.stringify(await(db.any('SELECT * FROM public."users" WHERE username=$1;', [username])));
+		return true;
+	} catch(e){
+		console.log(e);
+		return false;
 	}
 	// return exists;
 };
-
-//Produces a user identifier; puts it in req.session.passport.user = {id:''
-//ONLY saves the ID, not the user's personal cal id or anything else
-// Convert user object to a unique identifier.
-// passport.serializeUser((user, done) => {
-//     //user will come in as username
-// 	let userObj;
-// 	try{
-// 		userObj = JSON.stringify(db.any('SELECT * FROM public."users" WHERE username=$1;', [user]));
-// 		let user_id = userObj.id;
-// 		done(null, user_id);
-// 	} catch(e){
-// 		done(null, false);
-// 	}
-// 	// console.log(user);
-// 	//get userid from fetch
-// 	// const userObj = fetch('/api/username/'+user);
-
-// });
-
-// //take that identifier, make it into the user object
-// passport.deserializeUser((user_id, done) =>{
-// 	let user;
-//     try{ //try to get user id
-//         user = JSON.stringify(db.any('SELECT * FROM public."users" WHERE id=$1;', [user_id]));
-// 		// user = fetch(`/api/users/${user_id}`);
-// 	} catch {
-// 		return done(null, false);
-// 	}
-
-// 	done(null, user);
-// });
 
 
 exports.check = async function checkCreds(username, pwd){
 //get the user
 // if it fails, return false
 //otherwise, check password
-	let user;
+	// let user;
+	console.log('checking cred');
 	try { //check that user exists by username, returns user
-
-        user = JSON.stringify(db.any('SELECT * FROM public."users" WHERE username=$1;', [username]));
+		const user = (await(db.any('SELECT * FROM public."users" WHERE username=$1;', [username])));
 		//then check password
-		if(user.password !== pwd){
+		if(user[0].password_val !== pwd){
 			return false;
 		}
 	} catch (e){
-		console.log(e);
 		return false;
 	}
-
 	return true;
 };
 
